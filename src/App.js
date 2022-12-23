3b66c481d85e7f3e242714bf7abbcbffd84e3f1f
Cesar Aguilar <Blueavian9@gmail.com>
2022-12-22 20:11:34 -0800

    initial

--- src/App.js
+++ src/App.js
@@ -47,51 +47,11 @@ const App = () => {
 
       <Switch>
         <Route exact path="/" component={Home} />
-        <Route path="/pizza" component={Pizza} />
-        <Route path="/about" component={About} />
-        <Route path="/contact" component={Contact} />
+        <Route exact path="/pizza" component={Pizza} />
+        <Route exact path="/about" component={About} />
+        <Route exact path="/contact" component={Contact} />
       </Switch>
       <h2>Build your own Pizza</h2>
-      <label htmlFor="pizza">
-        <h3>Choice of Size:</h3>
-      </label>
-      <form id="pizza-form">
-        <label htmlFor="pizza-form">
-          <select name="order-pizza">
-            <option value="small">small</option>
-            <option value="medium">medium</option>
-            <option value="large">large</option>
-            <option value="extra-large">extra large</option>
-          </select>
-        </label>
-        <h2>Toppings</h2>
-
-        <label htmlFor="topping1">
-          topping1: <input id="topping1" type="checkbox" />
-        </label>
-        <label htmlFor="topping2">
-          topping2: <input id="topping2" type="checkbox" />
-        </label>
-        <label htmlFor="topping3">
-          topping3: <input id="topping3" type="checkbox" />
-        </label>
-
-        <h2>Choice of Substitute</h2>
-        <p>Choose one Topping per pizza order</p>
-        {/*Form input goes Here */}
-
-        <label htmlFor="fname">First Name: </label>
-        <input
-          type="name-input"
-          id="fname"
-          name="fname"
-          placeholder="Enter First Name"
-          onChange={(e) => setFormValues(e.target.value)}
-        />
-        <Link to="/pizza" id="order-pizza">
-          <button>Click!</button>
-        </Link>
-      </form>
     </div>
   );
 };
--- src/Pizza.js
+++ src/Pizza.js
@@ -4,6 +4,46 @@ const Pizza = () => {
   return (
     <div>
       <h1>Order Pizza!</h1>
+      <label htmlFor="pizza">
+        <h3>Choice of Size:</h3>
+      </label>
+      <form id="pizza-form">
+        <label htmlFor="pizza-form">
+          <select name="order-pizza">
+            <option value="small">small</option>
+            <option value="medium">medium</option>
+            <option value="large">large</option>
+            <option value="extra-large">extra large</option>
+          </select>
+        </label>
+        <h2>Toppings</h2>
+
+        <label htmlFor="topping1">
+          topping1: <input id="topping1" type="checkbox" />
+        </label>
+        <label htmlFor="topping2">
+          topping2: <input id="topping2" type="checkbox" />
+        </label>
+        <label htmlFor="topping3">
+          topping3: <input id="topping3" type="checkbox" />
+        </label>
+
+        <h2>Choice of Substitute</h2>
+        <p>Choose one Topping per pizza order</p>
+        {/*Form input goes Here */}
+
+        <label htmlFor="fname">First Name: </label>
+        <input
+          type="name-input"
+          id="fname"
+          name="fname"
+          placeholder="Enter First Name"
+          onChange={(e) => setFormValues(e.target.value)}
+        />
+        <Link to="/pizza" id="order-pizza">
+          <button>Click!</button>
+        </Link>
+      </form>
     </div>
   );
 };
